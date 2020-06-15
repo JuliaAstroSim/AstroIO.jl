066f163e63d2861c6d3dd6b93547005feca3a306
islent <leoislent@gmail.com>
2020-06-15 22:27:17 +0800

    backup [skip ci]

--- src/Houdini.jl
+++ src/Houdini.jl
@@ -6,15 +6,17 @@ function write_houdini_header(f::IOStream, pos::Array{T,N}) where T<:AbstractPar
     write(f, "id,Px,Py,Pz,Vx,Vy,Vz,time\n")
 end
 
-function write_houdini_data(f::IOStream, pos::Array{T,N}, time::Float64, units = uAstro) where T<:AbstractPoint3D where N
+function write_houdini_data(f::IOStream, pos::Array{T,N}, time::Float64, units = uAstro;
+                            pos_ratio = 1.0,
+                            ) where T<:AbstractPoint3D where N
     uLength = getuLength(units)
     for p in pos
         buffer = @sprintf(
             "%d,%f,%f,%f,%f\n",
             p.ID,
-            ustrip(uLength, p.x),
-            ustrip(uLength, p.y),
-            ustrip(uLength, p.z),
+            ustrip(uLength, p.x) * pos_ratio,
+            ustrip(uLength, p.y) * pos_ratio,
+            ustrip(uLength, p.z) * pos_ratio,
             time,
         )
         write(f, buffer)
@@ -22,7 +24,10 @@ function write_houdini_data(f::IOStream, pos::Array{T,N}, time::Float64, units =
     flush(f)
 end
 
-function write_houdini_data(f::IOStream, particles::Array{T,N}, time::Float64, units = uAstro) where T<:AbstractParticle3D where N
+function write_houdini_data(f::IOStream, particles::Array{T,N}, time::Float64, units = uAstro;
+                            pos_ratio = 1.0,
+                            vel_ratio = 1.0,
+                            ) where T<:AbstractParticle3D where N
     uLength = getuLength(units)
     uVel = getuVel(units)
 
@@ -31,12 +36,12 @@ function write_houdini_data(f::IOStream, particles::Array{T,N}, time::Float64, u
         buffer = @sprintf(
             "%d,%f,%f,%f,%f,%f,%f,%f\n",
             p.ID,
-            ustrip(uLength, p.Pos.x),
-            ustrip(uLength, p.Pos.y),
-            ustrip(uLength, p.Pos.z),
-            ustrip(uVel, p.Vel.x),
-            ustrip(uVel, p.Vel.y),
-            ustrip(uVel, p.Vel.z),
+            ustrip(uLength, p.Pos.x) * pos_ratio,
+            ustrip(uLength, p.Pos.y) * pos_ratio,
+            ustrip(uLength, p.Pos.z) * pos_ratio,
+            ustrip(uVel, p.Vel.x) * vel_ratio,
+            ustrip(uVel, p.Vel.y) * vel_ratio,
+            ustrip(uVel, p.Vel.z) * vel_ratio,
             time,
         )
         write(f, buffer)
@@ -62,7 +67,10 @@ end
 function write_houdini(OutputFile::AbstractString, folder::AbstractString, filenamebase::AbstractString,
                        Counts::Array{Int64,1}, suffix::AbstractString,
                        FileType::AbstractOutputType, units = uAstro;
-                       times = Counts,)
+                       times = Counts,
+                       time_ratio = 1.0,
+                       pos_ratio = 1.0,
+                       vel_ratio = 1.0,)
     output = open(OutputFile, "w")
     write(output, "id,Px,Py,Pz,Vx,Vy,Vz,time\n")
 
@@ -76,12 +84,14 @@ function write_houdini(OutputFile::AbstractString, folder::AbstractString, filen
             data = read_jld(input)
         end
 
-        write_houdini_data(output, data, times[i], units)
+        write_houdini_data(output, data, times[i] * time_ratio, units; pos_ratio = pos_ratio, vel_ratio = vel_ratio)
 
         next!(progress, showvalues = [("iter", i), ("time", times[i]), ("file", input)])
     end
     
+    close(output)
+    println("Data saved to ", OutputFile)
     return true
 end
 
-#write_houdini("HoudiniNiagara.csv", "output", "snapshot_", collect(0:10:100), ".gadget2", gadget2(), times = collect(0.0:0.01:0.1))
+#write_houdini("HoudiniNiagara.hcsv", "output", "snapshot_", collect(0:10:100), ".gadget2", gadget2(), times = collect(0.0:0.01:0.1))
