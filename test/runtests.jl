2ff135b664817b5e56b0c46b490b2928e34c3f35
Michele Mastropietro <michele.mastropietro@gmail.com>
2021-10-26 17:54:52 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,13 +25,35 @@ header, data = read_gadget2("gassphere_littleendian.gadget2") # 1472 gas particl
 
     pos = read_gadget2_pos("gadget2.format2")
     @test length(pos) == 1472
+    @test pos[1] == PVector(-0.07133729010820389*u"kpc", -0.35668644309043884*u"kpc", -0.9273847341537476*u"kpc")
 
     pos = read_gadget2_pos("gassphere_littleendian.gadget2")
     @test length(pos) == 1472
+    @test pos[1] == PVector(-0.07133729010820389*u"kpc", -0.35668644309043884*u"kpc", -0.9273847341537476*u"kpc")
+
+    uAcc = getuAcc(uGadget2)
+    uPot = getuEnergyUnit(uGadget2)
+    uMass = getuMass(uGadget2)
+
+    # @test 1.0*getuMass(uGadget2) == 1e10u"Msun"  # Still have to understand how to check this
+    @test uAcc == u"km^2*kpc^-1*s^-2"
+    @test uPot == u"km^2*s^-2"
 
     h, d = read_gadget2("pot_acc.format2.gadget2", acc = true, pot = true)
+    @test d.Acc[20] == PVector(1216.8760986328125*u"km^2*kpc^-1*s^-2",
+                               868.4943237304688*u"km^2*kpc^-1*s^-2",
+                               874.93798828125*u"km^2*kpc^-1*s^-2")
+    @test d.Potential[20] == -37.11515808105469uPot
+    @test d.Mass[20] == 9.99999993922529e-9uMass
+
     write_gadget2_format2("pot_acc.format2.test.gadget2", h, d, acc = true, pot = true)
+
     h, d = read_gadget2("pot_acc.format2.test.gadget2", acc = true, pot = true)
+    @test d.Acc[20] == PVector(1216.8760986328125*u"km^2*kpc^-1*s^-2",
+                               868.4943237304688*u"km^2*kpc^-1*s^-2",
+                               874.93798828125*u"km^2*kpc^-1*s^-2")
+    @test d.Potential[20] == -37.11515808105469uPot
+
     @test !iszero(norm(average(d, :Acc)))
     @test !iszero(norm(average(d, :Potential)))
 end
